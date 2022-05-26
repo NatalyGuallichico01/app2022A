180480b1e1e5c01fc5a138d9bbef0e1f7271ea82
NatalyGuallichico01 <natilu.naty@gmail.com>
2022-05-25 19:40:42 -0500

    ionic

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'a-ce494',
+    appId: '1:895765536527:web:5412a8377e447c54c7287a',
+    storageBucket: 'a-ce494.appspot.com',
+    apiKey: 'AIzaSyAsvKUpGAbfl6RdKJ-lTUYmqyb9XcZ_sqc',
+    authDomain: 'a-ce494.firebaseapp.com',
+    messagingSenderId: '895765536527',
+    measurementId: 'G-9M9K55KD0M',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'a-ce494',
+    appId: '1:895765536527:web:5412a8377e447c54c7287a',
+    storageBucket: 'a-ce494.appspot.com',
+    apiKey: 'AIzaSyAsvKUpGAbfl6RdKJ-lTUYmqyb9XcZ_sqc',
+    authDomain: 'a-ce494.firebaseapp.com',
+    messagingSenderId: '895765536527',
+    measurementId: 'G-9M9K55KD0M',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
